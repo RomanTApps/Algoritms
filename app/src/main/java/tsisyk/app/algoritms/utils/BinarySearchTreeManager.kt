42cf916c61dcfac0f2a8fd5c47447080bf896ecd
Roman Tsisyk <roman.tsisyk1@gmail.com>
2020-05-03 20:09:02 +0200

    add GraphTraversals , ShortestPathFinder, CycleDetector, TreeTraversals, BinarySearchTreeManage

---
+++ app/src/main/java/tsisyk/app/algoritms/utils/BinarySearchTreeManager.kt
@@ -0,0 +1,35 @@
+package tsisyk.app.algoritms.utils
+
+class BinarySearchTreeManager {
+    data class TreeNode(val value: Int, var left: TreeNode? = null, var right: TreeNode? = null)
+
+    var root: TreeNode? = null
+
+    fun insert(value: Int) {
+        root = insertRec(root, value)
+    }
+
+    private fun insertRec(current: TreeNode?, value: Int): TreeNode {
+        if (current == null) return TreeNode(value)
+        if (value < current.value) {
+            current.left = insertRec(current.left, value)
+        } else if (value > current.value) {
+            current.right = insertRec(current.right, value)
+        }
+        return current
+    }
+
+    fun contains(value: Int): Boolean {
+        var current = root
+        while (current != null) {
+            if (value < current.value) {
+                current = current.left
+            } else if (value > current.value) {
+                current = current.right
+            } else {
+                return true
+            }
+        }
+        return false
+    }
+}
